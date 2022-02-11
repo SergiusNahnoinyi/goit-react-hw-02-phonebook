5d923f1208a2961685915c61f3799ab5b3a2dedb
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-11 10:01:07 +0200

    Добавляет начальную разметку компонента

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,30 +5,39 @@ export default class Form extends Component {
 
   static propTypes = {};
 
-  state = {};
+  state = {
+    contacts: [],
+    name: '',
+  };
 
   render() {
     return (
-      <form>
-        <label>
-          Имя
-          <input
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Фамилия
-          <input
-            type="text"
-            name="surename"
-            value={this.state.surename}
-            onChange={this.handleChange}
-          />
-        </label>
-      </form>
+      <section className="phonebook">
+        {/* Form */}
+        <form className="form">
+          <h1 className="title">Phonebook</h1>
+          <label className="label">
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button className="button" type="submit">
+            Add contact
+          </button>
+        </form>
+        {/* Contacts */}
+        <div className="contacts">
+          <h1 className="title">Contacts</h1>
+          <ul className="contacts-list">
+            <li className="item"></li>
+          </ul>
+        </div>
+      </section>
     );
   }
 }
