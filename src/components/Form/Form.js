06dc8359509d4a36566783ef4f0b141a8944672a
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-11 10:15:19 +0200

    Добавляет метод обработки события на инпуте и обновления состояния компонента

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -10,6 +10,12 @@ export default class Form extends Component {
     name: '',
   };
 
+  handleChange = e => {
+    // this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <section className="phonebook">
@@ -21,6 +27,8 @@ export default class Form extends Component {
             <input
               type="text"
               name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
