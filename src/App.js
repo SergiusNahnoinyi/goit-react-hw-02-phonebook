69d3cc7287670f13ef9931d60e1db80a769a1d89
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-12 09:57:22 +0200

    Реализует поле поиска, которое можно использовать для фильтрации списка контактов по имени

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ export default class App extends Component {
   static propTypes = {};
 
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -22,9 +27,21 @@ export default class App extends Component {
     this.setState({ filter: e.currentTarget.value });
   };
 
+  getFiltredContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLocaleLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter),
+    );
+  };
+
   InputId = nanoid();
 
   render() {
+    const { filter } = this.state;
+    const filtredContacts = this.getFiltredContacts();
+
     return (
       <div className="App">
         <Form formData={this.formSubmitHandler} />
@@ -38,14 +55,14 @@ export default class App extends Component {
             type="text"
             name="name"
             id={this.InputId}
-            value={this.state.filter}
+            value={filter}
             onChange={this.changeFilter}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
           <ul className="contacts-list">
-            {this.state.contacts.map(contact => (
+            {filtredContacts.map(contact => (
               <li key={contact.id} className="item">
                 {contact.name}: {contact.number}
               </li>
