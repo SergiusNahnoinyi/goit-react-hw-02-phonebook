ca07e189904c53bab8d0ec81d54ee928a2e87f8e
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-12 10:00:27 +0200

    Выполняет деструктуризацию

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -17,9 +17,10 @@ export default class Form extends Component {
   };
 
   handleSubmit = e => {
+    const { formData } = this.props;
     e.preventDefault();
     const id = nanoid();
-    this.props.formData({ ...this.state, id });
+    formData({ ...this.state, id });
     this.reset();
   };
 
@@ -31,6 +32,7 @@ export default class Form extends Component {
   numberInputId = nanoid();
 
   render() {
+    const { name, number } = this.state;
     return (
       <section className="phonebook">
         {/* Form */}
@@ -43,7 +45,7 @@ export default class Form extends Component {
             type="text"
             name="name"
             id={this.nameInputId}
-            value={this.state.name}
+            value={name}
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -56,7 +58,7 @@ export default class Form extends Component {
             type="tel"
             name="number"
             id={this.numberInputId}
-            value={this.state.number}
+            value={number}
             onChange={this.handleChange}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
