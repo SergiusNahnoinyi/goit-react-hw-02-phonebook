1c8231fe8815a5120a552627067abdc2c19f03c0
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-12 08:46:54 +0200

    Добавляет инпут-фильтр по имени и добавляет метод для записи значения в state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
+import { nanoid } from 'nanoid';
 
 export default class App extends Component {
   static defaultProps = {};
@@ -8,6 +9,7 @@ export default class App extends Component {
 
   state = {
     contacts: [],
+    filter: '',
   };
 
   formSubmitHandler = formData => {
@@ -16,6 +18,12 @@ export default class App extends Component {
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  InputId = nanoid();
+
   render() {
     return (
       <div className="App">
@@ -23,6 +31,19 @@ export default class App extends Component {
         {/* Contacts */}
         <div className="contacts">
           <h1 className="title">Contacts</h1>
+          <label className="label" htmlFor={this.InputId}>
+            Find contact by name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id={this.InputId}
+            value={this.state.filter}
+            onChange={this.changeFilter}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
           <ul className="contacts-list">
             {this.state.contacts.map(contact => (
               <li key={contact.id} className="item">
