a42b4e5e8216372c07d457f49c54f4e86caab3f8
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-13 19:46:12 +0200

    Стилизирует компонент Filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import s from './Filter.module.css';
 
 export default function Filter({ stateFilterValue, inputValue }) {
   return (
-    <label className="label">
+    <label className={s.label}>
       Find contact by name
       <input
+        className={s.input}
         type="text"
         name="name"
         value={stateFilterValue}
