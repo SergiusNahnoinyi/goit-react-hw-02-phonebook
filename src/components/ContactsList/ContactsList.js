f9aa3545232fbbdbf043ea77aa659bec90b49d16
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-02-14 08:37:46 +0200

    Стилизирует компонент ContactsList и вносит финальные правки

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import s from './Contacts.module.css';
 
 export default function ContactsList({ contacts, onDelete }) {
   return (
-    <ul className="contacts-list">
+    <ul className={s.list}>
       {contacts.map(contact => (
-        <li key={contact.id} className="item">
+        <li key={contact.id} className={s.item}>
           {contact.name}: {contact.number}
           <button onClick={() => onDelete(contact.id)}>Delete</button>
         </li>
